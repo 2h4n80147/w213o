355fe42393d4420ea084ef2d9fa485746dbd5367
2h4n80147 <33758315+2h4n80147@users.noreply.github.com>
2023-04-27 12:03:15 +0600

    Add files via upload

---
+++ asdfjkasdjfkl.py
@@ -0,0 +1,42 @@
+from telethon import TelegramClient, sync, events
+
+api_id =  819745
+api_hash = 'b62e3c87a48c8ecef7d53ddc901f3c11'
+
+client = TelegramClient('session_name', api_id, api_hash)
+
+@client.on(events.NewMessage(chats=('chat_name')))
+async def normal_handler(event):
+#    print(event.message)
+    user_mess=event.message.to_dict()['message']
+
+    s_user_id=event.message.to_dict()['from_id']
+    user_id=int(s_user_id)
+    user=d.get(user_id)
+
+    mess_date=event.message.to_dict()['date']
+
+    f.write(mess_date.strftime("%d-%m-%Y %H:%M")+"\n")
+    f.write(user+"\n")
+    f.write(user_mess+"\n\n")
+
+    f.flush()
+
+client.start()
+
+group='goodwin_bets'
+
+participants = client.get_participants(group)
+users={}
+
+for partic in client.iter_participants(group):
+    lastname=""
+    print(partic.first_name, partic.id)
+    if partic.last_name:
+       lastname=partic.last_name
+    users[partic.id]=str(partic.first_name)+" "+lastname
+
+f=open('messages_from_chat', 'a') 
+
+client.run_until_disconnected()
+f.close()
